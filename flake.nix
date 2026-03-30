8ac4d697723fb9457f61c7524da855c012636ea8
Jun-Hong Cheng <qwe854896@gmail.com>
2026-03-30 22:25:56 +0800

    feat: scaffold flake with minimal nixvim config

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Standalone Nixvim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, nixvim, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { system, ... }:
+        let
+          nixvimLib = nixvim.lib.${system};
+          nixvimModule = {
+            inherit system;
+            module = import ./config;
+          };
+          nvim = nixvim.legacyPackages.${system}.makeNixvimWithModule nixvimModule;
+        in
+        {
+          packages.default = nvim;
+          checks.default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+        };
+    };
+}
