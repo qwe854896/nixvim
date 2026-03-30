5d74bbd716947204948bc6ead868a6769a150b2c
Jun-Hong Cheng <qwe854896@gmail.com>
2026-03-30 22:30:31 +0800

    fix: reduce flake scaffold to current system

--- flake.nix
+++ flake.nix
@@ -14,12 +14,7 @@
 
   outputs = inputs@{ flake-parts, nixvim, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+      systems = [ "x86_64-linux" ];
 
       perSystem = { system, ... }:
         let
